39748146c486b2dd7c6e134d771974c9895ca943
Karan09542 <karan09542@gmail.com>
2025-11-03 14:24:15 +0530

    done5

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,66 +1,61 @@
 import React, { useRef, useState } from "react";
 
 const Login = () => {
-
-    const users = [
-        {
-            id: 1,
-            name: "ABC",
-            email: "abc@gmail.com",
-            password: "12"
-        },
-        {
-            id: 2,
-            name: "DEF",
-            email: "def@gmail.com",
-            password: "1234"
-        },
-        {
-            id: 3,
-            name: "GHI",
-            email: "ghi@gmail.com",
-            password: "123456"
-        }
-    ]
+  const users = [
+    {
+      id: 1,
+      name: "ABC",
+      email: "abc@gmail.com",
+      password: "12",
+    },
+    {
+      id: 2,
+      name: "DEF",
+      email: "def@gmail.com",
+      password: "1234",
+    },
+    {
+      id: 3,
+      name: "GHI",
+      email: "ghi@gmail.com",
+      password: "123456",
+    },
+  ];
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
   const [error, setError] = useState("");
-  function onSubmit(){
+  function onSubmit(e) {
+    e.preventDefault();
+
     const email = emailRef.current?.value.trim();
     const password = passwordRef.current?.value.trim();
 
-    setTimeout(()=>{
-        if(users[email]) {
-            if(password !== users[email].password){
-                setError("Password Incorrect");
-                console.log("Password Incorrect");
-            }
+    setTimeout(() => {
+      const user = users.filter((user) => user.email === email);
+      if (user.length === 0) {
+        setError("User not found");
+        console.log("User not found");
+      } else {
+        if (password !== user[0].password) {
+          setError("Password Incorrect");
+          console.log("Password Incorrect");
+        } else {
+          setError("");
+          console.log(email, password);
         }
-        else if(!users[email]) {
-            setError("User not found");
-            console.log("User not found");
-        }
-        else {
-            setError("");
-            console.log(email, password)
-        };
-    },3000)
+      }
+    }, 3000);
   }
 
   return (
-    <div>
+    <form onSubmit={onSubmit}>
       <label htmlFor="input-email">Email</label>
-      <br/>
-      <input
-        ref={emailRef}
-        type="email"
-        placeholder="email"
-        id="input-email"
-      />
+      <br />
+      <input ref={emailRef} type="email" placeholder="email" id="input-email" />
       <br />
       <label htmlFor="input-password">Password</label>
-      <br/>
+      <br />
       <input
         ref={passwordRef}
         type="text"
@@ -68,11 +63,13 @@ const Login = () => {
         id="input-password"
       />
       <br />
-      <button onClick={onSubmit} id="submit-form-btn">Submit</button>
-      <br/>
+      <button onClick={onSubmit} id="submit-form-btn">
+        Submit
+      </button>
+      <br />
       {error === "User not found" && <h1 id="user-error">{error}</h1>}
       {error === "Password Incorrect" && <h1 id="password-error">{error}</h1>}
-    </div>
+    </form>
   );
 };
 
